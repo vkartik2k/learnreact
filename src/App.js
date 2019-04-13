80de04b926f2d86bcae66a33900507e1f934a7b6
vkartik2k <vkartik2k@gmail.com>
2019-04-13 14:57:33 +0530

    learning about state

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ import { SSL_OP_NETSCAPE_DEMO_CIPHER_CHANGE_BUG } from 'constants';
 
 class App extends React.Component {
 
-  clicked(event){
-    console.log(event);
+  state = {
+    keywords: 'myword',
+    val : 'myval'
   }
-  
+  changeHandler(event) {
+    this.setState({
+      keywords: event.target.value
+    })
+  }
   render() {
     return (
-      <div className="click" onClick={this.clicked}>Click bait</div>
+      <div className="container">
+        <div className="title" >My React App</div>
+        <input onChange={this.changeHandler.bind(this)}/>
+        <div className="key">{this.state.keywords}</div>
+      </div>
     );
   }
 }

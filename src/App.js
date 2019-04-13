7a52275417b4ee2f307bcb7095cb49852e813d68
vkartik2k <vkartik2k@gmail.com>
2019-04-13 11:28:24 +0530

    learning

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { SSL_OP_NETSCAPE_DEMO_CIPHER_CHANGE_BUG } from 'constants';
 
-class App extends Component {
+//inline styling
+
+class App extends React.Component {
+  
   render() {
+    let styles = {
+      container : {
+        backgroundColor : 'red'
+      }
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      React.createElement('div',{className: 'main',style : styles.container}, "Hello React")
     );
   }
 }
+
 
 export default App;
